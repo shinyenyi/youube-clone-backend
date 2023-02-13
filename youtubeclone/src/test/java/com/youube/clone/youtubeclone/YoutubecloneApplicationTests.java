883346814b57704467474a8a397e124ae7ce7cc8
Miguel Shinyenyi <miguelshinyenyi@gmail.com>
2023-02-13 15:52:30 +0300

    initialise springboot project

---
+++ youtubeclone/src/test/java/com/youube/clone/youtubeclone/YoutubecloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youube.clone.youtubeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubecloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
